6df841726c1947721db7a65de89fa1ebb210870e
LeonardodeAraujoAlves <78648608+LeonardodeAraujoAlves@users.noreply.github.com>
2022-09-02 15:37:38 -0300

    Add files via upload
    Algoritmo de ordenação Bubblesort implementado em c++

---
+++ bubblesort.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+int main(){
+	int a[]{7,1,3,15,0,2,4,5,8,6};
+	int tamanho{10};
+	
+	for(int i = 0; i < tamanho; i++){
+		for(int j = 0; j < (tamanho - 1 ); j++){
+			if(a[j] > a[j + 1]){
+				int temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+				
+			}
+		}
+	}
+	for(int i = 0; i < tamanho; i ++){
+		std::cout<< i<<" = "<< a[i]<<"\n";
+	}
+	return 0;
+}
